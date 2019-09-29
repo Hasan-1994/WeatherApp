0fb34a350a968e05870f168d1e35436cafcdfd78
Hasan Sivrikaya <35102813+HasanSivrikaya94@users.noreply.github.com>
2019-09-30 00:29:07 +0200

    WeatherApp

--- App.js
+++ App.js
@@ -5,78 +5,84 @@ var styles = require('./source/components/style/style')
 
 export default class App extends Component {
   state = {
-     lat: 52.517,
-     lon: 13.3883,
-     isLoading: true,
-     temperature: 0,
-     name: ''
+      lat: 52.517,
+      lon: 13.3883,
+      isLoading: true,
+      temperature: 0,
+      name: ''
   }
-  //Für überschreiben der Latetude und Logitude wird eine Standortgenämigung gebraucht. 
-  //Hierzu kommt die Methode navigator.geolocation.getCurrentPosition ins Spiel.
+  /**
+   * Für überschreiben der Latetude und Logitude wird eine Standortgenämigung gebraucht.
+   * Hierzu kommt die Methode navigator.geolocation.getCurrentPosition ins Spiel.
+   */
   componentDidMount() {
-     navigator.geolocation.getCurrentPosition(
-        position => {
-           this.fetchWeather(this.state.lat, this.state.lon);
-        },
-     );
+      navigator.geolocation.getCurrentPosition(
+          position => {
+              this.fetchWeather(this.state.lat, this.state.lon);
+          },
+      );
   }
-  //Daten werden aus der API gezogen.  Latetude und Longitude werden über die State runtergzogen,
-  //mein ApiKey wird ebenfalls eingefügt.
+
+  /**
+   * Daten werden aus der API gezogen.  Latetude und Longitude werden über die State runtergzogen,
+   * mein ApiKey wird ebenfalls eingefügt.
+   */
   fetchWeather(lat = this.state.lat, lon = this.state.lon) {
-    fetch(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${ApiKey}&units=metric`
-   ).then(res => res.json())
-   .then(json => {
-      this.setState({
-         temperature: json.main.temp,
-         name: json.name,
-         isLoading: false,
-      });
-   });
-  
-  }   
+      fetch(
+              `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${ApiKey}&units=metric`
+          ).then(res => res.json())
+          .then(json => {
+              this.setState({
+                  temperature: json.main.temp,
+                  name: json.name,
+                  isLoading: false,
+              });
+          });
+
+  } 
   render() {
-    const { isLoading } = this.state
-    function timer()
-    {while(isLoading === true){
-      setTimeout(function(){Alert.alert('Error', 'Please check your Location Values')},5000)
+     const {isLoading}  = this.state;
+        function timer(){
+      if(isLoading === true){
+        setTimeout(function(){Alert.alert('Error', 'Please check your Location Values')},5000);
+      }else{
+        {isLoading}
+      }
+      // while(isLoading == true){
+      //   setTimeout(function(){Alert.alert('Error', 'Please check your Location Values')},5000);
+      // }
     }
-  }
-    // function timer(){
-    //   if(isLoading === true){
-    //     setTimeout(function(){Alert.alert('Error', 'Please check your Location Values')},5000)
-    //   }else{
-    //     isLoading = {isLoading}
-    //   }
-    // }
     return (
       <View style={styles.mainContainer}>
-        
+
         <Text style={styles.header}>
           Wetter App
         </Text>
-        
-        <Image source={require('./source/components/assets/images/weather.png')} style={styles.icon}/>
-        
+
+        <Image source={require('./source/components/assets/images/weather.png')} style={styles.icon} />
+
 
         <View>
-        <Text style={styles.cityText}>
-        {this.state.name}
-        </Text>
+
+          <Text style={styles.cityText}>
+            {this.state.name}
+          </Text>
+
         </View>
 
-
         <View style={styles.mainContainer}>
+
+          {timer()}
           {isLoading ? (
-            timer(),
-            <ActivityIndicator size="large" color="#0000ff"/>
-          ) 
-          : 
+            <ActivityIndicator size="large" color="#0000ff" />
+          )
+          :
           (
               <Text style={styles.tempText}>
                 Die Tempertur beträgt {this.state.temperature} °C
-                </Text>
+              </Text>
           )}
+
         </View>
 
       </View>
--- source/components/style/style.js
+++ source/components/style/style.js
@@ -4,37 +4,38 @@ var { StyleSheet } = React;
 
 module.exports = StyleSheet.create({
     mainContainer: {
-        backgroundColor: '#679a9e',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1
-    },
-    tempContainer: {
-        height: '45%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    header: {
-        fontSize: 35,
-        fontFamily: 'bnjinx',
-        color: 'black'
-    },
-    tempText: {
-        fontSize: 22,
-        fontFamily: 'distant',
-        color: 'black'
-    },
-    cityText: {
-        fontSize: 20,
-        fontFamily: 'distant',
-        color: 'black'
-    },
-    icon: {
-        width: 200,
-        height: 200
-    },
-    btnContainer:{
-        width: 100,
-        height: 10
-    }
+   	 backgroundColor: '#679a9e',
+     alignItems: 'center',
+     justifyContent: 'center',
+     flex: 1
+ },
+ tempContainer: {
+   	 height: '45%',
+     alignItems: 'center',
+     justifyContent: 'center',
+ },
+ header: {
+  	 fontSize: 35,
+     fontFamily: 'bnjinx',
+     color: 'black'
+ },
+ tempText: {
+   fontSize: 22,
+     fontFamily: 'distant',
+     color: 'black'
+ },
+ cityText: {
+  	 fontSize: 20,
+     fontFamily: 'distant',
+     color: 'black'
+ },
+ icon: {
+  	 width: 200,
+     height: 200
+ },
+ btnContainer: {
+  	 width: 100,
+     height: 10
+ }
+
 });
